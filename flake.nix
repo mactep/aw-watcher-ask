03799abe052f6afa4987e6df775cc7d421937004
mactep <brunoro.tulio@gmail.com>
2025-10-17 02:20:31 -0300

    feat: add Nix flake for packaging

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "ActivityWatch watcher that asks the user questions";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = {
+          default = (pkgs.callPackage ./default.nix { }).aw-watcher-ask;
+        };
+      }
+    );
+}
